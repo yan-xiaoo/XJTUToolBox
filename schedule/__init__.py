3b3773a6d75972b8bc6afb54751d0ec76a494a5f
Li Yanxiao <liyanxiao050804@126.com>
2024-12-03 21:48:07 +0800

    chore: 提升依赖库版本和 Python 版本
    升级了 requests, PyQt-Fluent-Widgets, pycryptodome 的版本。
    由于 Python 3.8 已经不再受支持，且 Windows 7 早已不受支持，更新 Python 版本到 3.12。从此版本后，Windows 版本要求提升为 Windows 8.1。
    此外，修正了错误的引用尚未提交的 Python 文件的问题。
    
    BREAKING CHANGE: 提升 Python 版本到 3.12

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -1,4 +1,4 @@
 from .lesson import Lesson
 from .schedule import Schedule, WeekSchedule, DaySchedule
-from .attendance_lesson import AttendanceState, AttendanceLesson, updateLatestFlow
-from .xjtu_time import getAttendanceStartTime, getClassStartTime, getClassEndTime, getAttendanceEndTime
+# from .attendance_lesson import AttendanceState, AttendanceLesson, updateLatestFlow
+# from .xjtu_time import getAttendanceStartTime, getClassStartTime, getClassEndTime, getAttendanceEndTime
