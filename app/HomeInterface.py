8df40f4c0e642dba60e20fdab8ad4ee9ad2c27cf
yeliqin666 <liurunqin666@163.com>
2026-08-18 13:51:33 +0800

    feat: 教师主页检索与轻解析

--- app/HomeInterface.py
+++ app/HomeInterface.py
@@ -198,6 +198,13 @@ class HomeFrame(QWidget):
                 'content': self.tr("查看作业、课件与课程回放"),
                 'callback': lambda: self.main_window.switchTo(self.main_window.lms_interface),
                 'color': LinkCard.LinkCardColor.SKY_BLUE
+            },
+            "faculty": {
+                'icon': FIF.EDUCATION.icon(theme=Theme.DARK),
+                'title': self.tr("教师主页"),
+                'content': self.tr("检索教师主页"),
+                'callback': lambda: self.main_window.switchTo(self.main_window.faculty_interface),
+                'color': LinkCard.LinkCardColor.SKY_BLUE
             }
         }
 
--- app/main_window.py
+++ app/main_window.py
@@ -32,6 +32,7 @@ from .sessions.gste_session import GSTESession
 from .sessions.jwapp_session import JwappSession
 from .sessions.lms_session import LMSSession
 from .sessions.venue_session import VenueSession
+from .FacultyInterface import FacultyInterface
 from .sub_interfaces import LoginInterface
 from .sub_interfaces.QRCodeLoginDialog import QRCodeLoginDialog
 from .sub_interfaces.VerifyCodeDialog import VerifyCodeDialog
@@ -202,6 +203,8 @@ class MainWindow(MSFluentWindow):
         QApplication.processEvents()
         self.venue_interface = VenueInterface(self)
         QApplication.processEvents()
+        self.faculty_interface = FacultyInterface(self)
+        QApplication.processEvents()
 
         self.tray_interface = TrayInterface(QIcon("assets/icons/main_icon.ico"))
         self.tray_interface.main_interface.connect(lambda: self.show())
@@ -363,6 +366,12 @@ class MainWindow(MSFluentWindow):
         empty_room_card = self.tool_box_interface.addCard(self.empty_room_interface, FIF.LAYOUT, self.tr("空闲教室"),
                                                           self.tr("查询当前空闲的教室"))
         empty_room_card.setFixedSize(200, 180)
+
+        faculty_card = self.tool_box_interface.addCard(
+            self.faculty_interface, FIF.EDUCATION, self.tr("教师主页"),
+            self.tr("检索教师主页"),
+        )
+        faculty_card.setFixedSize(200, 180)
 
         # 添加登录界面作为子界面，但是将其隐藏
         button = self.addSubInterface(self.login_interface, FIF.SCROLL, self.tr("登录"),
